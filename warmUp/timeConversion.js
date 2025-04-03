704d5b536862e5a0dba13676e545c6a44e58da3d
IvanderKendrick <ken100840@gmail.com>
2025-04-03 10:34:39 +0700

    Solved 3 - 3/4/2025

---
+++ warmUp/timeConversion.js
@@ -0,0 +1,55 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'timeConversion' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts STRING s as parameter.
+ */
+
+function timeConversion(s) {
+  // Write your code here
+  let [hour, minute, sec] = s.slice(0, -2).split(":");
+  hour = parseInt(hour);
+
+  let period = s.slice(-2);
+
+  if (period == "AM" && hour == 12) hour = 0;
+  if (period == "PM" && hour != 12) hour += 12;
+
+  return `${hour.toString().padStart(2, "0")}:${minute}:${sec}`;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const s = readLine();
+
+  const result = timeConversion(s);
+
+  ws.write(result + "\n");
+
+  ws.end();
+}
