e62f8ae8a588bfd12dbe5c8653aaa6d0afa6e112
IvanderKendrick <ken100840@gmail.com>
2025-05-03 18:58:45 +0700

    Solved 1 - 3/5/2025

---
+++ implementation/cutTheSticks.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const fs = require("fs");
+
+process.stdin.resume();
+process.stdin.setEncoding("utf-8");
+
+let inputString = "";
+let currentLine = 0;
+
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n");
+
+  main();
+});
+
+function readLine() {
+  return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'cutTheSticks' function below.
+ *
+ * The function is expected to return an INTEGER_ARRAY.
+ * The function accepts INTEGER_ARRAY arr as parameter.
+ */
+
+function cutTheSticks(arr) {
+  let answer = [];
+
+  arr.sort((a, b) => a - b);
+
+  while (arr.length > 0) {
+    answer.push(arr.length);
+    let min = arr[0];
+    arr = arr.map((x) => x - min).filter((x) => x > 0);
+  }
+
+  return answer;
+}
+
+function main() {
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+  const n = parseInt(readLine().trim(), 10);
+
+  const arr = readLine()
+    .replace(/\s+$/g, "")
+    .split(" ")
+    .map((arrTemp) => parseInt(arrTemp, 10));
+
+  const result = cutTheSticks(arr);
+
+  ws.write(result.join("\n") + "\n");
+
+  ws.end();
+}
